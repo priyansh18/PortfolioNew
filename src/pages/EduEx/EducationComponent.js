e006761bc507de27d4b85fccef4c762485f4ea1d
Abhishek Jaglan <jaglanabhishek08@gmail.com>
2024-08-16 19:15:20 +0530

    unused certification declaration

--- src/pages/EduEx/EducationComponent.js
+++ src/pages/EduEx/EducationComponent.js
@@ -3,7 +3,7 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 // import Educations from "../../containers/education/Educations";
-import Certifications from "../../containers/certifications/Certifications";
+// import Certifications from "../../containers/certifications/Certifications";
 import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
 import EducationImg from "./EducationImg";
 import { competitiveSites } from "../../portfolio";
